e595c3a43b7938bf8a99c3615512ca3bb0d3238e
Tridip Rong <59462773+TridipRong@users.noreply.github.com>
2023-04-15 15:23:18 +0530

    updated

--- script.js
+++ script.js
@@ -71,6 +71,91 @@ $(document).ready(function () {
     },
   });
 });
+//Home content animation
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
+};
+
+const texts = [
+  "Hi..",
+  "My Name Is",
+  "Tridip Rong",
+  "I'm a",
+  "Java Back-End Developer",
+  "Coder",
+  ":)",
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+animate();
 
 // Email
 
@@ -88,3 +173,22 @@ function sendmail() {
     });
   document.getElementById("formm").reset();
 }
+// resume button
+document.getElementById("resume-btn").addEventListener("click", function () {
+  // Replace 'resume.pdf' with the name of your resume file
+  var fileUrl = "/images/Tridip_Rong_Resume_.pdf";
+
+  // Download the file
+  var a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileUrl.split("/").pop();
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  // Open the Google Drive link in a new window/tab
+  window.open(
+    "https://drive.google.com/file/d/10lxA17GoED_pAOCwvu6YxyUdUDzWch27/view",
+    "_blank"
+  );
+});
